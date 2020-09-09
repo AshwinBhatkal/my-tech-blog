6cc783b042ea674183803f6eb2a54f3dd7f23509
Ashwin Bhatkal <aub001n@homeoffice.wal-mart.com>
2020-09-09 11:49:50 +0530

    Updated hero image size

--- components/heroImage.js
+++ components/heroImage.js
@@ -3,7 +3,7 @@ import { minWidthMQ } from "../styles/mediaQueries";
 
 const StyledImage = styled.img`
     border-radius: 50%/50%;
-    width: 200px;
+    width: 300px;
     max-width: 100%;
 
     ${minWidthMQ[1]} {
